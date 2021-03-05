3084b1d2d0d74df42641cb740293b01bcf24f44f
Us0457 <71816792+Us0457@users.noreply.github.com>
2021-03-05 20:35:42 +0530

    Create HOME_AUTOMATION.js

---
+++ HOME_AUTOMATION.js
@@ -0,0 +1,16 @@
+singleButton({name:"load1 On", action:"digitalWrite",
+pin:"1", value:"HIGH"})
+singleButton({name:"load1 Off", action:"digitalWrite",
+pin:"1", value:"LOW"})
+singleButton({name:"load2 On", action:"digitalWrite",
+pin:"2", value:"HIGH"})
+singleButton({name:"load2 Off", action:"digitalWrite",
+pin:"2", value:"LOW"})
+singleButton({name:"load3 On", action:"digitalWrite",
+pin:"3", value:"HIGH"})
+singleButton({name:"load3 Off", action:"digitalWrite",
+pin:"3", value:"LOW"})
+singleButton({name:"load4 On", action:"digitalWrite",
+pin:"4", value:"HIGH"})
+singleButton({name:"load4 Off", action:"digitalWrite",
+pin:"4", value:"LOW"})
